873de5bf7c15253d712b27be1e3ffd5da8d06316
Passion-Logan <wangdadamailbox@163.com>
2019-09-06 16:24:12 +0800

    添加单向通信片段

--- Socket/src/OneWay/BaseSocketServer.java
+++ Socket/src/OneWay/BaseSocketServer.java
@@ -1,5 +1,6 @@
 package OneWay;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -23,5 +24,56 @@ public class BaseSocketServer
     private InputStream inputStream;
     private static final int MAX_BUFFE_SIZE = 1024;
 
-    
+    public int getPort()
+    {
+        return port;
+    }
+
+    public void setPort(int port)
+    {
+        this.port = port;
+    }
+
+    public BaseSocketServer(int port) {
+        this.port = port;
+    }
+
+    public void runServerSingle() throws IOException
+    {
+        this.server = new ServerSocket(this.port);
+
+        System.out.println("base socket server started.");
+        this.socket = server.accept();
+
+        this.inputStream = this.socket.getInputStream();
+
+        byte[] readBytes = new byte[MAX_BUFFE_SIZE];
+
+        int msgLen;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while ((msgLen = inputStream.read(readBytes)) != -1) {
+            stringBuilder.append(new String(readBytes, 0, msgLen, "UTF-8"));
+        }
+
+        System.out.println("get message from client: " + stringBuilder);
+
+        inputStream.close();
+        socket.close();
+        server.close();
+    }
+
+    public static void main(String[] args)
+    {
+        BaseSocketServer bs = new BaseSocketServer(9799);
+
+        try
+        {
+            bs.runServerSingle();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
