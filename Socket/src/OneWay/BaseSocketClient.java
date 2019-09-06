df0aff6f0ae729d4caff71132579e48e7dbf84cc
Passion-Logan <wangdadamailbox@163.com>
2019-09-06 16:30:12 +0800

    添加单向通信片段

---
+++ Socket/src/OneWay/BaseSocketClient.java
@@ -0,0 +1,54 @@
+package OneWay;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * @File Name: OneWay
+ * @Author: WQL //作者及
+ * @Date: 2019/9/6 16:25//完成日期
+ * @Description: // 单向通信客户端
+ * @Version: v0.0.1 // 版本信息
+ * @Function List: // 主要函数及其功能
+ * @Others: // 其它内容的说明
+ * @History: // 历史修改记录
+ */
+public class BaseSocketClient
+{
+
+    private String serverHost;
+    private int serverPort;
+    private Socket socket;
+    private OutputStream outputStream;
+
+    public BaseSocketClient(String host, int port) {
+        this.serverHost = host;
+        this.serverPort = port;
+    }
+
+    public void connetServer() throws IOException
+    {
+        this.socket = new Socket(this.serverHost, this.serverPort);
+        this.outputStream = socket.getOutputStream();
+    }
+
+    public void sendSingle(String message) throws IOException
+    {
+        try
+        {
+            this.outputStream.write(message.getBytes("UTF-8"));
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        this.outputStream.close();
+        this.socket.close();
+    }
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
