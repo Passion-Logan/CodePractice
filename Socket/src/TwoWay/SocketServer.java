fbcd8b5b24401105d1b30d8e54849e1a0f9b5336
Passion-Logan <wangdadamailbox@163.com>
2019-09-06 21:34:35 +0800

    添加双向通信片段

--- Socket/src/TwoWay/SocketServer.java
+++ Socket/src/TwoWay/SocketServer.java
@@ -1,5 +1,10 @@
 package TwoWay;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+
 /**
  * @File Name: TwoWay
  * @Author: WQL //作者及
@@ -12,4 +17,52 @@ package TwoWay;
  */
 public class SocketServer
 {
+
+    private ServerSocket server;
+    private Socket socket;
+    private int port;
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    public int getPort()
+    {
+        return port;
+    }
+
+    public void setPort(int port)
+    {
+        this.port = port;
+    }
+
+    public SocketServer(int port) {
+        this.port = port;
+    }
+
+    public void runServer() throws IOException
+    {
+        this.server = new ServerSocket(port);
+        this.socket = server.accept();
+        this.inputStream = socket.getInputStream();
+
+        byte[] readBytes = new byte[1024];
+
+        int msgLen;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while ((msgLen = inputStream.read(readBytes)) != -1) {
+            stringBuilder.append(new String(readBytes, 0, msgLen, "UTF-8"));
+        }
+
+        System.out.println("received message: " + stringBuilder);
+
+        // 告诉客户端已经接收完毕，之后只能发送
+        this.socket.shutdownInput();
+
+        this.outputStream = this.socket.getOutputStream();
+        String receipt = "We received your message: " + stringBuilder;
+        outputStream.write(receipt.getBytes("UTF-8"));
+
+        this.outputStream.close();
+        this.socket.close();
+    }
 }
