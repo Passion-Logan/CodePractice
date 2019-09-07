2b7b932deff8cb4f682c3ec7bb77e91c851b77bb
Passion-Logan <wangdadamailbox@163.com>
2019-09-07 22:41:37 +0800

    添加结束界定

---
+++ Socket/src/EndDefinition/SocketClient.java
@@ -0,0 +1,80 @@
+package EndDefinition;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * @File Name: TwoWay
+ * @Author: WQL //作者及
+ * @Date: 2019/9/6 21:34//完成日期
+ * @Description: // 双向通信客户端
+ * @Version: v0.0.1 // 版本信息
+ * @Function List: // 主要函数及其功能
+ * @Others: // 其它内容的说明
+ * @History: // 历史修改记录
+ */
+public class SocketClient
+{
+    private String serverHost;
+    private int serverPort;
+    private Socket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+
+    public SocketClient(String host, int port) {
+        this.serverHost = host;
+        this.serverPort = port;
+    }
+
+    public void connetServer() throws IOException
+    {
+        this.socket = new Socket(this.serverHost, this.serverPort);
+        this.outputStream = socket.getOutputStream();
+    }
+
+    public void sendMessage(String message) throws IOException
+    {
+        try
+        {
+            this.outputStream.write(message.getBytes("UTF-8"));
+            // 告诉服务器，所有的发送动作已经结束，之后只能接收
+            this.socket.shutdownOutput();
+
+            this.inputStream = socket.getInputStream();
+
+            byte[] readBytes = new byte[1024];
+
+            int msgLen;
+            StringBuilder receipt = new StringBuilder();
+
+            while ((msgLen = inputStream.read(readBytes)) != -1) {
+                receipt.append(new String(readBytes, 0, msgLen, "UTF-8"));
+            }
+
+            System.out.println("get receipt: " + receipt.toString());
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        this.inputStream.close();
+        this.outputStream.close();
+        this.socket.close();
+    }
+
+    public static void main(String[] args)
+    {
+        SocketClient sc = new SocketClient("localhost", 9799);
+        try
+        {
+            sc.connetServer();
+            sc.sendMessage("Hi from Socket");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Socket/src/EndDefinition/SocketServer.java
@@ -0,0 +1,83 @@
+package EndDefinition;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @File Name: TwoWay
+ * @Author: WQL //作者及
+ * @Date: 2019/9/6 17:34//完成日期
+ * @Description: // 双向通信服务端
+ * @Version: v0.0.1 // 版本信息
+ * @Function List: // 主要函数及其功能
+ * @Others: // 其它内容的说明
+ * @History: // 历史修改记录
+ */
+public class SocketServer
+{
+
+    private ServerSocket server;
+    private Socket socket;
+    private int port;
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    public int getPort()
+    {
+        return port;
+    }
+
+    public void setPort(int port)
+    {
+        this.port = port;
+    }
+
+    public SocketServer(int port) {
+        this.port = port;
+    }
+
+    public void runServer() throws IOException
+    {
+        this.server = new ServerSocket(port);
+        this.socket = server.accept();
+        this.inputStream = socket.getInputStream();
+
+        byte[] readBytes = new byte[1024];
+
+        int msgLen;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while ((msgLen = inputStream.read(readBytes)) != -1) {
+            stringBuilder.append(new String(readBytes, 0, msgLen, "UTF-8"));
+        }
+
+        System.out.println("received message: " + stringBuilder.toString());
+
+        // 告诉客户端已经接收完毕，之后只能发送
+        this.socket.shutdownInput();
+
+        this.outputStream = this.socket.getOutputStream();
+        String receipt = "We received your message: " + stringBuilder.toString();
+        outputStream.write(receipt.getBytes("UTF-8"));
+
+        this.outputStream.close();
+        this.socket.close();
+    }
+
+    public static void main(String[] args)
+    {
+        SocketServer ss = new SocketServer(9799);
+
+        try
+        {
+            ss.runServer();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
