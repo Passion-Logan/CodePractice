8f9f5accce9b1757c0d1cf072d3926df9d28b01e
passion-logan <wangdadamailbox@163.com>
2019-10-11 10:18:20 +0800

    添加修改

--- quartz/src/main/java/com/cody/QuartzApplication.java
+++ quartz/src/main/java/com/cody/QuartzApplication.java
@@ -3,6 +3,9 @@ package com.cody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wdd
+ */
 @SpringBootApplication
 public class QuartzApplication {
 
