02f160155796a1af6ad760a9be28ebc946a96ca7
passion-logan <wangdadamailbox@163.com>
2019-10-12 14:15:22 +0800

    添加定时任务CRUD

---
+++ quartz_crud/src/main/java/com/cody/QuartzCrudApplication.java
@@ -0,0 +1,13 @@
+package com.cody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzCrudApplication.class, args);
+    }
+
+}
