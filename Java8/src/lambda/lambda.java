4a191760e4e6a6c617afdaa7e75e1bab66d898f3
passion-logan <wangdadamailbox@163.com>
2019-10-17 17:07:57 +0800

    添加修改

---
+++ Java8/src/lambda/lambda.java
@@ -0,0 +1,16 @@
+package lambda;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
+ *
+ * @author WQL
+ * @since 2019年10月17日 0017 17:07
+ */
+public class lambda {
+
+    public static void main(String[] args) {
+        
+    }
+}
