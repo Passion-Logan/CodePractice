09666610bc6e858538d55d0d3d8c882e6991db86
passion-logan <wangdadamailbox@163.com>
2019-10-17 17:38:03 +0800

    添加修改

--- Java8/src/lambda/lambda.java
+++ Java8/src/lambda/lambda.java
@@ -16,16 +16,17 @@ public class lambda {
 
     public static void main(String[] args) {
         List<String> list = Arrays.asList("你好", "世界", "hello", "world");
-        //filter(list, (str) -> ("hello".equals(str)));
-        //filter(list, (str) -> (((String) str).length() == 2));
+        filter(list, (str) -> ("hello".equals(str)));
+        filter(list, (str) -> (((String) str).length() == 2));
 
         // 如果需要同时满足两个条件或满足其中一个即可，Predicate可以将这样的多个条件合并成一个。
-        Predicate start = (str) -> (((String) str).startsWith("你好"));
-        Predicate len = (str) -> (((String) str).length() == 2);
+        Predicate start = (str) -> (((String) str).startsWith("0"));
+        Predicate len = (str) -> (((String) str).length() == 5);
 
-        // 满族一个条件
+        // 满足一个条件
         list.stream().filter(start).forEach(System.out::println);
-        // 
+        // 满足两个条件中的一个
+        list.stream().filter(start.or(len)).forEach(System.out::println);
 
 
     }
