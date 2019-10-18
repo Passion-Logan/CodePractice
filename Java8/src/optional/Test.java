8ec96d3868f93a8fd0422f0409beac5d08e56956
passion-logan <wangdadamailbox@163.com>
2019-10-18 14:28:00 +0800

    添加修改

--- Java8/src/optional/Test.java
+++ Java8/src/optional/Test.java
@@ -9,4 +9,20 @@ package optional;
  * @since 2019年10月18日 0018 14:22
  */
 public class Test {
+
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * 老式判断非空写法
+     * @return
+     */
+    public static String getPersonName(Person person) {
+        if (person != null) {
+            return person.getName();
+        } else {
+            return "--";
+        }
+    }
 }
