d0d122e8468cf52a207e61578d67d60bc42e1101
passion-logan <wangdadamailbox@163.com>
2019-10-18 14:21:04 +0800

    添加修改

---
+++ Java8/src/optional/Person.java
@@ -0,0 +1,27 @@
+package optional;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
+ *
+ * @author WQL
+ * @since 2019年10月18日 0018 14:17
+ */
+public class Person {
+
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+
+}
