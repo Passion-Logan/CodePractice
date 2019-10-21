a7c8f653cc3a8f23e2b56db52d1cf991c3127854
passion-logan <wangdadamailbox@163.com>
2019-10-21 16:00:02 +0800

    添加修改

--- global_exception/src/main/java/com/cody/exception/ApiResultGenerator.java
+++ global_exception/src/main/java/com/cody/exception/ApiResultGenerator.java
@@ -2,6 +2,8 @@ package com.cody.exception;
 
 import com.cody.entity.ApiResult;
 
+import java.util.List;
+
 /**
  * 应用模块名称<p>
  * 代码描述<p>
@@ -11,9 +13,36 @@ import com.cody.entity.ApiResult;
  * @since 2019年10月21日 0021 15:17
  */
 public final class ApiResultGenerator {
+    /**
+     * 创建普通消息方法
+     * @param flag
+     * @param msg
+     * @param result
+     * @param jumpUrl
+     * @param rows
+     * @param throwable
+     * @return
+     */
     public static ApiResult result(boolean flag, String msg, Object result, String jumpUrl, int rows, Throwable throwable) {
         // 创建返回对象
         ApiResult apiResult = ApiResult.newInstance();
-        apiResult.
+        apiResult.setFlag(true);
+        apiResult.setMsg(msg == "" ? "执行成功" : msg);
+        apiResult.setResult(result);
+        apiResult.setJumpUrl(jumpUrl);
+        apiResult.setTime(System.currentTimeMillis());
+        apiResult.setRows(rows);
+        return apiResult;
+    }
+
+    public static ApiResult successResult(Object result) {
+        // rows默认为0
+        int rows = 0;
+        // 如果是集合
+        if (result instanceof List) {
+            // 获取总数量
+            rows = ((List) result).size();
+        }
+        return result(true,"",result,"",rows,null);
     }
 }
