2e6a4262668882bb9638ff1e38cb935978b0108e
passion-logan <wangdadamailbox@163.com>
2019-10-21 14:35:32 +0800

    添加修改

---
+++ global_exception/src/main/java/com/cody/exception/RestExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.cody.exception;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
+ *
+ * @author WQL
+ * @since 2019年10月21日 0021 14:34
+ */
+public class RestExceptionHandler {
+}
