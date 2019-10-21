fd80f26ae03158c0d56bca1c17ed8453673cc686
passion-logan <wangdadamailbox@163.com>
2019-10-21 14:48:16 +0800

    添加修改

--- global_exception/src/main/java/com/cody/exception/RestExceptionHandler.java
+++ global_exception/src/main/java/com/cody/exception/RestExceptionHandler.java
@@ -1,12 +1,19 @@
 package com.cody.exception;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
- * 应用模块名称<p>
+ * result统一异常处理<p>
+ *     具体Controller不需要进行单独异常处理
+ *     由该类统一处理
  * 代码描述<p>
  * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
  *
  * @author WQL
  * @since 2019年10月21日 0021 14:34
  */
+@ControllerAdvice
+@ResponseBody
 public class RestExceptionHandler {
 }
