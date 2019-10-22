041cd07abfcd0504cddd94403d11f3862526500a
passion-logan <wangdadamailbox@163.com>
2019-10-22 16:48:35 +0800

    添加测试类

--- rabbitmq/src/main/java/com/cody/rabbitmq/Sender.java
+++ rabbitmq/src/main/java/com/cody/rabbitmq/Sender.java
@@ -20,6 +20,10 @@ public class Sender {
     @Autowired
     private AmqpTemplate rabbitTemplate;
 
+    /**
+     * 生产者，使用AmqpTemplate接口操作
+     * AmqpTemplate接口定义了一套针对AMQP协议的基础操作
+     */
     public void send() {
         String context = "hello " + new Date();
         System.out.println("Sender : " + context);
