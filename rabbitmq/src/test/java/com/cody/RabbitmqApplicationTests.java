ed761acf264f114ac880b1fa0e4412f053df9d75
passion-logan <wangdadamailbox@163.com>
2019-10-22 16:46:50 +0800

    添加修改

--- rabbitmq/src/test/java/com/cody/RabbitmqApplicationTests.java
+++ rabbitmq/src/test/java/com/cody/RabbitmqApplicationTests.java
@@ -1,10 +1,20 @@
 package com.cody;
 
+import com.cody.rabbitmq.Sender;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RabbitmqApplicationTests {
+
+    @Autowired
+    private Sender sender;
+
+    @Test
+    public void hello() {
+        sender.send();
+    }
 
     @Test
     void contextLoads() {
