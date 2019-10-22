517b936e515afb575fe511c2fb552e1d1224356c
passion-logan <wangdadamailbox@163.com>
2019-10-22 16:29:56 +0800

    添加修改

---
+++ rabbitmq/src/main/java/com/cody/config/RabbitConfig.java
@@ -0,0 +1,12 @@
+package com.cody.config;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
+ *
+ * @author WQL
+ * @since 2019年10月22日 0022 16:29
+ */
+public class RabbitConfig {
+}
