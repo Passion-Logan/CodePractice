d5747cb253aac1a41476c5fb3e01ce55df1bb4ea
passion-logan <wangdadamailbox@163.com>
2019-10-22 16:37:12 +0800

    添加修改

---
+++ rabbitmq/src/main/java/com/cody/rabbitmq/Sender.java
@@ -0,0 +1,12 @@
+package com.cody.rabbitmq;
+
+/**
+ * 生产者<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
+ *
+ * @author WQL
+ * @since 2019年10月22日 0022 16:36
+ */
+public class Sender {
+}
