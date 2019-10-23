445b3b41c9a09ef55f54fbcd568d70ea19d213c4
passion-logan <wangdadamailbox@163.com>
2019-10-23 14:25:56 +0800

    消费者消息确认处理

---
+++ rabbitmq/src/main/java/com/cody/config/MessageListenerConfig.java
@@ -0,0 +1,15 @@
+package com.cody.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 消费者消息确认机制处理<p>
+ * 代码描述<p>
+ * Copyright: Copyright (C) 2019 XXX, Inc. All rights reserved. <p>
+ *
+ * @author WQL
+ * @since 2019年10月23日 0023 14:24
+ */
+@Configuration
+public class MessageListenerConfig {
+}
