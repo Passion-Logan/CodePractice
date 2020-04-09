79101d362718d13b40b718143d0be500b28290f1
Passion-Logan <wangdadamailbox@163.com>
2020-04-10 00:15:48 +0800

    添加基本说明

---
+++ regex/src/start.java
@@ -0,0 +1,25 @@
+/**
+ * ClassName: start
+ *
+ * @author WQL
+ * @Description:
+ * @date: 2020/4/10 0:01
+ * @since JDK 1.8
+ */
+public class start {
+
+    /**
+     * 正则表达式开始：
+     * 例子：^[a-z0-9_-]{3,15}&
+     * 开始标记：^
+     * 结束标记：&
+     * 字母：a-z
+     * 数字：0-9
+     * 3-15个字符长度：3,15
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
