052562724d5e11a6b959d3682a2a7f5bb1a3d527
Passion-Logan <wangdadamailbox@163.com>
2020-04-10 23:57:47 +0800

    添加修改

--- regex/src/two.java
+++ regex/src/two.java
@@ -10,9 +10,10 @@ public class two {
 
     /**
      * 元字符讲解：
-     * 1 点运算符 .
+     * 1 点运算符 .是元字符中最简单的例子。 .匹配任意单个字符，但不匹配换行符。 例如，表达式.ar匹配一个任意字符后面跟着是a和r的字符串。
      *          ".ar" => The car parked in the garage.   匹配 car par gar
-     * 2 字符集 []
+     * 2 字符集 [] 字符集也叫做字符类。 方括号用来指定一个字符集。 在方括号中使用连字符来指定字符集的范围。
+     * 在方括号中的字符集不关心顺序。 例如，表达式[Tt]he 匹配 the 和 The。
      *          "[Tt]he" => The car parked in the garage. 匹配 The the
      */
 }
