4bbfd197948209f25b2ff4f27ba44bf6ede89f48
Passion-Logan <wangdadamailbox@163.com>
2020-04-10 23:56:50 +0800

    添加元字符例子

---
+++ regex/src/two.java
@@ -0,0 +1,18 @@
+/**
+ * ClassName: two
+ *
+ * @author WQL
+ * @Description:
+ * @date: 2020/4/10 23:52
+ * @since JDK 1.8
+ */
+public class two {
+
+    /**
+     * 元字符讲解：
+     * 1 点运算符 .
+     *          ".ar" => The car parked in the garage.   匹配 car par gar
+     * 2 字符集 []
+     *          "[Tt]he" => The car parked in the garage. 匹配 The the
+     */
+}
